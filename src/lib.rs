e34efce119bdc26dedf8702f5ae615b9d053cc16
Hayley Hughes <hayley@foxes.systems>
2021-09-13 11:45:48 +1000

    Convert to using async

--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,28 @@
 //!
 //! # Example
 //! ```rust
-//! extern crate canihazip;
-//! # use std::net::Ipv4Addr;
-//!
-//! # fn main() -> Result<(), reqwest::Error> {
-//! let ip = canihazip::plz_ip()?;
-//!
-//! assert_eq!(ip.is_loopback(), false);
+//! # async fn run() -> Result<(), reqwest::Error> {
+//! let ip = canihazip::plz_ip().await?;
 //! # Ok(())
 //! # }
 //! ```
+
+#[cfg(test)]
+extern crate tokio;
+
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 pub type Error = reqwest::Error;
 
+
 /// Get your current public IP address.
 ///
 /// Panics if the returned IP is not a valid IP address.
-pub fn plz_ip() -> Result<IpAddr, Error> {
-    Ok(reqwest::blocking::get("https://icanhazip.com/")?
-        .text()?
+pub async fn plz_ip() -> Result<IpAddr, Error> {
+    Ok(reqwest::get("https://icanhazip.com/")
+        .await?
+        .text()
+        .await?
         .trim()
         .parse::<IpAddr>()
         .expect("Error parsing IP address"))
@@ -31,9 +33,11 @@ pub fn plz_ip() -> Result<IpAddr, Error> {
 /// Get your current public IPv4 address.
 ///
 /// Panics if the returned IP is not a valid IPv4 address.
-pub fn plz_ipv4() -> Result<Ipv4Addr, Error> {
-    Ok(reqwest::blocking::get("https://4.icanhazip.com/")?
-        .text()?
+pub async fn plz_ipv4() -> Result<Ipv4Addr, Error> {
+    Ok(reqwest::get("https://4.icanhazip.com/")
+        .await?
+        .text()
+        .await?
         .trim()
         .parse::<Ipv4Addr>()
         .expect("Error parsing IPv4 address"))
@@ -43,9 +47,11 @@ pub fn plz_ipv4() -> Result<Ipv4Addr, Error> {
 ///
 /// Errors if there is no Ipv6 connectivity.
 /// Panics if the returned IP is not a valid IPv6 address.
-pub fn plz_ipv6() -> Result<Ipv6Addr, Error> {
-    Ok(reqwest::blocking::get("https://6.icanhazip.com/")?
-        .text()?
+pub async fn plz_ipv6() -> Result<Ipv6Addr, Error> {
+    Ok(reqwest::get("https://6.icanhazip.com/")
+        .await?
+        .text()
+        .await?
         .trim()
         .parse::<Ipv6Addr>()
         .expect("Error parsing IPv6 address"))
@@ -55,17 +61,17 @@ pub fn plz_ipv6() -> Result<Ipv6Addr, Error> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_plz_ip() {
-        let ip = plz_ip().unwrap();
+    #[tokio::test]
+    async fn test_plz_ip() {
+        let ip = plz_ip().await.unwrap();
 
         assert_eq!(ip.is_loopback(), false);
         assert_eq!(ip.is_unspecified(), false);
     }
 
-    #[test]
-    fn test_plz_ipv4() {
-        let ip = plz_ipv4().unwrap();
+    #[tokio::test]
+    async fn test_plz_ipv4() {
+        let ip = plz_ipv4().await.unwrap();
 
         assert_eq!(ip.is_private(), false);
         assert_eq!(ip.is_loopback(), false);
@@ -75,10 +81,10 @@ mod tests {
         assert_eq!(ip.is_unspecified(), false);
     }
 
-    #[test]
+    #[tokio::test]
     #[ignore]
-    fn test_plz_ipv6() {
-        let ip = plz_ipv6().unwrap();
+    async fn test_plz_ipv6() {
+        let ip = plz_ipv6().await.unwrap();
 
         assert_eq!(ip.is_loopback(), false);
         assert_eq!(ip.is_unspecified(), false);
